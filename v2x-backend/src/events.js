cd9797a84da9761cb8070aa30a21de06c0f28b8d
SharunKrish <sharunkrishna28@gmail.com>
2026-03-14 12:46:02 +0530

    first running version

---
+++ v2x-backend/src/events.js
@@ -0,0 +1,11 @@
+const EVENTS = {
+  TELEMETRY: 'telemetry',
+  ALERT: 'alert',
+  BRAKE_HAZARD: 'brake_hazard',
+  SOS: 'sos',
+  RESET: 'reset',
+  RESET_ACK: 'reset_ack',
+  START_DEMO: 'start_demo'
+};
+
+module.exports = EVENTS;
